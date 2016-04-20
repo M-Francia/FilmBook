f17eed6b5708095d873cc71e4b2df6470f489846
Nazaret7 <nazaretfyq@hotmail.com>
2016-04-20 16:49:58 +0200

    Visualización html

--- Angular/src/app/cabecera.component.ts
+++ Angular/src/app/cabecera.component.ts
@@ -1,14 +1,26 @@
-import {Component} from 'angular2/core';
+import {Component,Input,Output,EventEmitter} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {RegistroComponent} from './registro.component.ts';
+import {usuario} from './Fichapelicula/usuario.model';
 @Component({
        selector: 'cabecera', // mirar que es selector
        templateUrl: 'app/cabecera.component.html',
-       directives:[ROUTER_DIRECTIVES]
 })
 export class CabeceraComponent  {
-  usuario = "";
-  contraseña = "";
 
-  log (){ //linea 41
+  usuario='';
+  password='';
+
+/*    log(usuario:string, contraseña:string){
+    for(var usuario of this.usuarios){
+      if(usuario.nombre==usuario){
+        if(usuario.contraseña==contraseña){
+          alert('Se ha logueado');
+          return undefined;
+        }
+      }
+    }
+     alert('El usuario y/o la contraseña introducidas no son correctas');
   }
+*/
 }
